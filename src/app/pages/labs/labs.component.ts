d6d30381cbdf512093513b88847a495b381b0341
llortegall <ivanortega_97@hotmail.com>
2025-05-11 18:36:05 +0000

    labs components

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -27,10 +27,60 @@ export class LabsComponent {
     }
   ];
 
+  task2Signals = signal([
+    {
+      id: 1,
+      title: 'Task 1',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Task 3',
+      completed: false
+    }
+  ])
+
+  nameSignal = signal('Ivan Ortega Whit Signal Example')
+
   name = 'Ivan Ortega';
   age = 28;
 
   items = ['Item 1', 'Item 2', 'Item 3'];
 
   disabled = true;
+
+  handleClick() {
+    alert('Click');
+  }
+
+  changeHandler(ev: Event) {
+    ev.preventDefault();
+    console.log(ev);
+    this.age = Number((ev.target as HTMLInputElement).value);
+  }
+
+  changeHandlerName(ev: Event) {
+    ev.preventDefault();
+    console.log(ev);
+    this.name = (ev.target as HTMLInputElement).value;
+  }
+
+  changeHandlerNameSignal(ev: Event) {
+    console.log(ev);
+    const input = ev.target as HTMLInputElement;
+    this.nameSignal.set(input.value);
+  }
+
+  // usar un solo handler para los eventos con multiple inputs
+  changeHandlerMultipleInputs(ev: Event) {
+    ev.preventDefault();
+    console.log(ev);
+    this.name = (ev.target as HTMLInputElement).value;
+    this.age = Number((ev.target as HTMLInputElement).value);
+  }
 }
