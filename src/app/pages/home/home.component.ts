d4b8364597743e0897d3d028325c3a3e08d5963f
llortegall <ivanortega_97@hotmail.com>
2025-05-11 19:01:48 +0000

    try fields

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { Task } from '@/app/models/task.model'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  task = signal<Task[]>([
+    {
+      id: 1,
+      title: 'User el CLI de angular',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Crear un proyecto de angular',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Crear un componente',
+      completed: false
+    }
+  ]);
 
+  addTask(event: Event){
+    event.preventDefault();
+    
+    const input = event.target as HTMLFormElement;
+    const newTask = input['taskName'].value;
+
+    this.task.update(tasks => [...tasks, { id: tasks.length + 1, title: newTask, completed: false }])
+    input['taskName'].value = '';
+  }
+
+  toggleTask(id: number){
+    this.task.update(tasks => tasks.map(task => task.id === id ? {...task, completed: !task.completed} : task))
+  }
+
+  deleteTask(id: number){
+    this.task.update(tasks => tasks.filter(task => task.id !== id))
+  }
 }
