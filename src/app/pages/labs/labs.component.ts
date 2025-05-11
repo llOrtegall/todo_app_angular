0ef85cf2b1e35672cfca32d56fde6fd51f5b8333
llortegall <ivanortega_97@hotmail.com>
2025-05-11 17:26:22 +0000

    add config.ts and add router outlel

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Todo App Angular Learning';
+  tasks = [
+    {
+      id: 1,
+      title: 'Task 1',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Task 2',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Task 3',
+      completed: false
+    }
+  ]
+}
