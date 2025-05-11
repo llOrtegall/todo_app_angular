d2ca849487db47476b38f899e263aa9c2ee56dc6
llortegall <ivanortega_97@hotmail.com>
2025-05-11 23:41:07 +0000

    editing task ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,17 +14,20 @@ export class HomeComponent {
     {
       id: 1,
       title: 'User el CLI de angular',
-      completed: false
+      completed: false,
+      editing: false
     },
     {
       id: 2,
       title: 'Crear un proyecto de angular',
-      completed: false
+      completed: false,
+      editing: false
     },
     {
       id: 3,
       title: 'Crear un componente',
-      completed: false
+      completed: false,
+      editing: false
     }
   ]);
 
@@ -41,38 +44,70 @@ export class HomeComponent {
   //   }
 
   //   this.task.update(tasks => [...tasks, { id: tasks.length +1, title: taskName, completed: false}]);
-    
+
   //   (event.target as HTMLFormElement).reset();
   // }
-    
 
-  toggleTask(id: number){
-    this.task.update(tasks => tasks.map(task => task.id === id ? {...task, completed: !task.completed} : task))
+
+  toggleTask(id: number) {
+    this.task.update(tasks => tasks.map(task =>
+      task.id === id
+        ? { ...task, completed: !task.completed }
+        : task
+    ))
   }
 
-  deleteTask(id: number){
+  deleteTask(id: number) {
     this.task.update(tasks => tasks.filter(task => task.id !== id))
   }
 
-  newTaskControl = new FormControl('', { 
+  newTaskControl = new FormControl('', {
     nonNullable: true,
     validators: [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(100)
     ]
-   });
+  });
 
-   addTask(event: Event){
+  addTask(event: Event) {
     event.preventDefault();
-    if(this.newTaskControl.valid){
+    if (this.newTaskControl.valid) {
       const value = this.newTaskControl.value.trim();
-      if(value === ''){
+      if (value === '') {
         alert('Por favor ingresa un nombre para la tarea');
         return;
       }
-      this.task.update(tasks => [...tasks, { id: tasks.length +1, title: value, completed: false}]);
+      this.task.update(tasks => [...tasks, { id: tasks.length + 1, title: value, completed: false, editing: false }]);
       this.newTaskControl.reset();
     }
-   }
+  }
+
+  updateTaskEditingMode(id: number) {
+    this.task.update(prevTasks => {
+      return prevTasks.map(task =>
+        task.id === id
+          ? { ...task, editing: !task.editing }
+          : { ...task, editing: false }
+      )
+    })
+  }
+
+  updateTaskTitle(ev: Event, id: number) {
+    ev.preventDefault();
+
+    const value = (ev.target as HTMLFormElement)['updateTitle'].value.trim();
+    if (value === '') {
+      alert('Por favor ingresa un nombre para la tarea');
+      return;
+    }
+    this.task.update(prevTasks => {
+      return prevTasks.map(task =>
+        task.id === id
+          ? { ...task, title: value, editing: false }
+          : task
+      )
+    })
+  }
+
 }
